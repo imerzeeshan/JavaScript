c2d3e5b47b6ae980c6f0faddbecd78765d431b35
MD ZEESHAN <imerzeeshan@gmail.com>
2024-03-28 01:34:43 +0530

    Project On - Color Changing

---
+++ 09_Project/01-unlimitedColorChanging/unlimitedColors.js
@@ -0,0 +1,26 @@
+// generate a random color
+
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color
+};
+let intervalId;
+const stratChangingColor = function(){
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor,1000);
+    }
+    
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+};
+document.querySelector('#start').addEventListener('click',stratChangingColor);
+document.querySelector('#stop').addEventListener('click',stopChangingColor);
